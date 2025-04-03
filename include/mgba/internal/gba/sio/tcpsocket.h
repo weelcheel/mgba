cb8b00350ded61ce2b2973f89f0e08a8c2b9ccb1
weelcheel <placeholder@email.com>
2025-04-03 03:07:00 -0500

    #1: add libwebsocket

--- include/mgba/internal/gba/sio/tcpsocket.h
+++ include/mgba/internal/gba/sio/tcpsocket.h
@@ -11,9 +11,9 @@ CXX_GUARD_START
 #include <mgba-util/socket.h>
 
 enum TcpState {
-    TCP_STATE_INIT,
     TCP_STATE_HANDSHAKE,
-    TCP_STATE_CONNECT,
+    TCP_STATE_INIT,
+    TCP_STATE_INIT_URL_TRANSFER,
     TCP_STATE_CONNECTING,
     TCP_STATE_CONNECTED,
     TCP_STATE_DISCONNECTED,
@@ -22,20 +22,29 @@ enum TcpState {
 struct GBASIOTCPSocket {
     struct GBASIODriver driver;
     struct mTimingEvent event;
-    
-    Socket tcpSocket;
-    struct Address* address;
-    short port;
+
+    // websocket functions
+    uint8_t* (*receive)(struct GBASIOTCPSocket* tcp);
+    void (*send)(struct GBASIOTCPSocket* tcp, uint8_t* data, uint32_t length);
+    bool (*connect)(struct GBASIOTCPSocket* tcp, const uint8_t* url);
+    void (*disconnect)(struct GBASIOTCPSocket* tcp);
 
     bool isActive;
-    bool isConnected;
     enum TcpState state;
-    enum TcpState nextState;
+
+    bool hasReceivedValidHandshake;
+
+    bool hasReceivedUrlHeader;
+    uint16_t urlLength;
+    uint16_t urlRecvPosition;
+    uint8_t* url;
+
+    uint32_t result;
 };
 
 extern const uint16_t TCP_PORT;
 
-void GBASIOTCPSocketCreate(struct GBASIOTCPSocket*);
+void GBASIOTCPSocketCreate(struct GBASIOTCPSocket* tcp);
 void GBASIOTCPSocketDestroy(struct GBASIOTCPSocket*);
 bool GBASIOTCPSocketConnect(struct GBASIOTCPSocket*);
 bool GBASIOTCPSocketIsConnected(struct GBASIOTCPSocket*);
--- src/gba/sio/tcpsocket.c
+++ src/gba/sio/tcpsocket.c
@@ -3,6 +3,16 @@
 #include <mgba/internal/gba/gba.h>
 #include <mgba/internal/gba/io.h>
 
+#include <libwebsockets.h>
+
+#define BITS_PER_SECOND 115200
+#define CYCLES_PER_BIT (GBA_ARM7TDMI_FREQUENCY / BITS_PER_SECOND)
+#define CLOCK_GRAIN (CYCLES_PER_BIT * 8)
+
+#define TCP_HANDSHAKE   0xBAC7
+#define TCP_URL_META    0xBAC8
+#define TCP_URL_HEADER  0xBAC9
+
 const uint16_t TCP_PORT = 6969;
 
 static bool GBASIOTCPSocketInit(struct GBASIODriver* driver);
@@ -11,7 +21,6 @@ static void GBASIOTCPSocketSetMode(struct GBASIODriver* driver, enum GBASIOMode 
 static bool GBASIOTCPSocketHandlesMode(struct GBASIODriver* driver, enum GBASIOMode mode);
 static int GBASIOTCPSocketConnectedDevices(struct GBASIODriver* driver);
 static void GBASIOTCPSocketProcessEvents(struct mTiming* timing, void* context, uint32_t cyclesLate);
-static uint16_t GBASIOTCPSocketWriteSIOCNT(struct GBASIODriver* driver, uint16_t value);
 static bool GBASIOTCPSocketStart(struct GBASIODriver* driver);
 static uint32_t GBASIOTCPSocketFinishNormal32(struct GBASIODriver* driver);
 
@@ -30,37 +39,18 @@ void GBASIOTCPSocketCreate(struct GBASIOTCPSocket* tcp) {
     tcp->event.name = "GB SIO TCP Socket";
     tcp->event.callback = GBASIOTCPSocketProcessEvents;
     tcp->event.priority = 0x80;
-
-    tcp->isActive = false;
-    tcp->state = TCP_STATE_INIT;
-    tcp->nextState = TCP_STATE_INIT;
-    tcp->isConnected = false;
-
-    // debug
-    tcp->address = (struct Address*) malloc(sizeof(struct Address));
-    tcp->address->version = IPV4;
-    struct in_addr ip_addr;
-    inet_pton(AF_INET, "127.0.0.1", &(ip_addr.s_addr));
-    tcp->address->ipv4 = ip_addr.s_addr;
-    tcp->port = TCP_PORT;
 }
 
 void GBASIOTCPSocketDestroy(struct GBASIOTCPSocket* tcp) {
     mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Destroying SIO TCP socket driver...");
-    if (!SOCKET_FAILED(tcp->tcpSocket)) {
-        SocketClose(tcp->tcpSocket);
-        tcp->tcpSocket = INVALID_SOCKET;
-    }
 }
 
 bool GBASIOTCPSocketConnect(struct GBASIOTCPSocket* tcp) {
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Setting SIO TCP socket data...");
     return tcp->driver.init != NULL;
 }
 
 bool GBASIOTCPSocketIsConnected(struct GBASIOTCPSocket* tcp) {
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Getting is connected...");
-    return tcp->isActive;
+    return true;
 }
 
 // ================== PRIVATE FUNCTIONS ==================
@@ -77,16 +67,28 @@ static void GBASIOTCPSocketReset(struct GBASIODriver* driver) {
 
     // @TODO: reset socket
     struct GBASIOTCPSocket* tcp = (struct GBASIOTCPSocket*) driver;
+
+    tcp->state = TCP_STATE_HANDSHAKE;
+    tcp->hasReceivedValidHandshake = false;
+    tcp->hasReceivedUrlHeader = false;
+    tcp->urlLength = 0;
+    tcp->result = 0;
     tcp->isActive = false;
+
     mTimingDeschedule(&tcp->driver.p->p->timing, &tcp->event);
 	mTimingSchedule(&tcp->driver.p->p->timing, &tcp->event, 0);
-    tcp->nextState = TCP_STATE_HANDSHAKE;
 }
 
 static void GBASIOTCPSocketSetMode(struct GBASIODriver* driver, enum GBASIOMode mode) {
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Setting mode in SIO TCP socket...");
+    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Setting mode in SIO TCP socket to %d...", mode);
     struct GBASIOTCPSocket* tcp = (struct GBASIOTCPSocket*) driver;
-    tcp->isActive = mode == GBA_SIO_NORMAL_32;
+
+    if (mode == GBA_SIO_NORMAL_32 && driver->start) {
+        tcp->state = TCP_STATE_INIT;
+        tcp->isActive = true;
+    } else {
+        mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Unsupported mode %d", mode);
+    }
 }
 
 static bool GBASIOTCPSocketHandlesMode(struct GBASIODriver* driver, enum GBASIOMode mode) {
@@ -100,80 +102,98 @@ static int GBASIOTCPSocketConnectedDevices(struct GBASIODriver* driver) {
 }
 
 static void GBASIOTCPSocketProcessEvents(struct mTiming* timing, void* context, uint32_t cyclesLate) {
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Process events callback...");
-}
+    struct GBASIOTCPSocket* tcp = (struct GBASIOTCPSocket*) context;
+   
+    //@TODO: send and receive websocket frames
 
-static uint16_t GBASIOTCPSocketWriteSIOCNT(struct GBASIODriver* driver, uint16_t value) {
-	UNUSED(driver);
-	mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: SIOCNT <- %04X", value);
-	return value;
+    int32_t nextEvent = CLOCK_GRAIN;
+    mTimingSchedule(timing, &tcp->event, nextEvent);
 }
 
 static bool GBASIOTCPSocketStart(struct GBASIODriver* driver) {
+    // process data received from the gba
     struct GBASIOTCPSocket* tcp = (struct GBASIOTCPSocket*) driver;
-    uint32_t value = driver->p->p->memory.io[GBA_REG(SIODATA32_LO)] | (driver->p->p->memory.io[GBA_REG(SIODATA32_HI)] << 16);
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Received value <- %04X", value);
 
-    switch (tcp->state)
-    {
+    if (!tcp->isActive) {
+        return false;
+    }
+
+    uint32_t inValue = tcp->driver.p->p->memory.io[GBA_REG(SIODATA32_LO)] | (tcp->driver.p->p->memory.io[GBA_REG(SIODATA32_HI)] << 16);
+    switch (tcp->state) {
         case TCP_STATE_HANDSHAKE:
-            if (value == 0xBAC7)
-            {
-                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Handshake complete");
-                tcp->nextState = TCP_STATE_CONNECTING;
+            // expect the gba to send the handshake value, then send it back when it is received
+            mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Checking handshake value %04X", inValue);
+            if (inValue == TCP_HANDSHAKE) {
+                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Handshake value is valid");
+                tcp->hasReceivedValidHandshake = true;
+                tcp->state = TCP_STATE_INIT_URL_TRANSFER;
+                tcp->result = TCP_HANDSHAKE;
+            } else {
+                tcp->hasReceivedValidHandshake = false;
+                return false;
             }
-            else
-            {
-                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Unexpected value during handshake.");
+            break;
+        case TCP_STATE_INIT:
+            // break down the 32 bit inValue to two 16 bit integers
+            // if the first 16 bits are the URL header, then the second 16 bits are the URL string length
+            uint16_t values[2] = { inValue & 0xFFFF, (inValue >> 16) & 0xFFFF };
+            if (values[0] == TCP_URL_META) {
+                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Received TCP_URL_META expecting a URL of length %d", values[1]);
+                tcp->urlLength = values[1];
+                tcp->state = TCP_STATE_INIT_URL_TRANSFER;
+                tcp->result = TCP_URL_META;
+                tcp->hasReceivedUrlHeader = false;
+            }
+            else {
+                tcp->state = TCP_STATE_INIT;
+                tcp->urlLength = 0;
+                return false;
+            }
+            break;
+        case TCP_STATE_INIT_URL_TRANSFER:
+            if (tcp->hasReceivedUrlHeader) {
+                // break the incoming 32 bit integer down into four 8 bit characters
+                // keep reading 8 bit characters until the expected urlLength is reached
+                uint8_t url[4] = { inValue & 0xFF, (inValue >> 8) & 0xFF, (inValue >> 16) & 0xFF, (inValue >> 24) & 0xFF };
+                uint8_t charsToRead = tcp->urlLength - (tcp->urlRecvPosition + 1);
+                if (charsToRead > 4) {
+                    charsToRead = 4;
+                }
+                for (uint8_t i = tcp->urlRecvPosition; i < tcp->urlRecvPosition + charsToRead; i++) {
+                    tcp->url[i] = url[i - tcp->urlRecvPosition];
+                }
+                tcp->urlRecvPosition += charsToRead;
+                if (tcp->urlRecvPosition >= tcp->urlLength) {
+                    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: URL transfer complete, value is %s", tcp->url);
+                    tcp->state = TCP_STATE_CONNECTING;
+                    tcp->result = TCP_URL_HEADER;
+                }
+                else {
+                    return false;
+                }
+            }
+            else if (inValue == TCP_URL_HEADER) {
+                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: URL header is valid, expecting string data of length %d", tcp->urlLength);
+                tcp->hasReceivedValidHandshake = true;
+                tcp->state = TCP_STATE_INIT_URL_TRANSFER;
+                tcp->result = TCP_URL_HEADER;
+                tcp->url = malloc(tcp->urlLength);
+                tcp->urlRecvPosition = 0;
+            }
+            else {
+                tcp->hasReceivedUrlHeader = false;
+                return false;
             }
             break;
         case TCP_STATE_CONNECTING:
-            tcp->isActive = true;
-            if (!tcp->isConnected) {
-                if (!SOCKET_FAILED(tcp->tcpSocket)) {
-                    SocketClose(tcp->tcpSocket);
-                    tcp->tcpSocket = INVALID_SOCKET;
-                }
-                if (!tcp->port) {
-                    tcp->port = TCP_PORT;
-                }
-
-                tcp->tcpSocket = SocketConnectTCP(tcp->port, tcp->address);
-                SocketSetBlocking(tcp->tcpSocket, false);
-                SocketSetTCPPush(tcp->tcpSocket, true);
-
-                struct in_addr ip_addr;
-                char str[INET_ADDRSTRLEN];
-                ip_addr.s_addr = tcp->address->ipv4;
-                inet_ntop(AF_INET, &(ip_addr.s_addr), str, INET_ADDRSTRLEN);
-                mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Opened socket to %s:%d", str, tcp->port);
-
-                tcp->nextState = TCP_STATE_CONNECTED;
-                tcp->isConnected = true;
-            }
             break;
     }
-
-    tcp->isActive = true;
     return true;
 }
 
 static uint32_t GBASIOTCPSocketFinishNormal32(struct GBASIODriver* driver) {
     struct GBASIOTCPSocket* tcp = (struct GBASIOTCPSocket*) driver;
-    uint32_t result = 0;
-    switch (tcp->state)
-    {
-        case TCP_STATE_HANDSHAKE:
-            result = 0xBAC7;
-            break;
-        case TCP_STATE_CONNECTING:
-            if (tcp->isConnected) {
-                result = 0xBAC8;
-            }
-            break;
-    }
-
-    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Finish normal32: %04X | Next state: %d", result, tcp->nextState);
-    tcp->state = tcp->nextState;
+    uint32_t result = tcp->result;
+    mLOG(GBA_SIO, DEBUG, "TCP Socket Driver: Finish 32 bit transfer - sending back %04X", result);
     return result;
 }
